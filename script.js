dd8e3910c7253da5bab23a6d79289d075022ed4e
KavinduLakshan393 <kavinduuni@gmail.com>
2026-08-15 23:26:35 +0530

    Commit 3: Game State and Players

--- script.js
+++ script.js
@@ -34,3 +34,58 @@ class Deck {
         }
     }
 }
+
+class Player {
+    constructor(id, name, isAI = true) {
+        this.id = id;
+        this.name = name;
+        this.isAI = isAI;
+        this.hand = [];
+    }
+
+    addCards(cards) {
+        this.hand.push(...cards);
+    }
+}
+
+class Team {
+    constructor(id, name, players) {
+        this.id = id;
+        this.name = name;
+        this.players = players;
+        this.tricksWon = 0;
+    }
+}
+
+class Game {
+    constructor() {
+        this.players = [
+            new Player(0, 'You', false),
+            new Player(1, 'Player 2 (Left)'),
+            new Player(2, 'Player 3 (Partner)'),
+            new Player(3, 'Player 4 (Right)')
+        ];
+        
+        this.teams = [
+            new Team(0, 'Team NS (You & P3)', [this.players[0], this.players[2]]),
+            new Team(1, 'Team EW (P2 & P4)', [this.players[1], this.players[3]])
+        ];
+        
+        this.deck = new Deck();
+        this.trumpSuit = null;
+        this.trumpCaller = null;
+        this.currentTrick = [];
+        this.turnIndex = 0;
+    }
+
+    initRound() {
+        this.deck = new Deck();
+        this.deck.shuffle();
+        this.teams.forEach(t => t.tricksWon = 0);
+        this.players.forEach(p => p.hand = []);
+        this.trumpSuit = null;
+        this.currentTrick = [];
+    }
+}
+
+const game = new Game();
