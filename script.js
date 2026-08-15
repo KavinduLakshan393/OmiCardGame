2a45bd9cd8bcd06c56cb35c23662442b1b576b5d
KavinduLakshan393 <kavinduuni@gmail.com>
2026-08-15 23:25:40 +0530

    Commit 2: Deck and Card logic

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Omi Card Game Initialized');
+const SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+const RANKS = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+class Card {
+    constructor(suit, rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+    
+    toString() {
+        return `${this.rank} of ${this.suit}`;
+    }
+}
+
+class Deck {
+    constructor() {
+        this.cards = this.buildDeck();
+    }
+
+    buildDeck() {
+        const deck = [];
+        for (const suit of SUITS) {
+            for (const rank of RANKS) {
+                deck.push(new Card(suit, rank));
+            }
+        }
+        return deck;
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+}
